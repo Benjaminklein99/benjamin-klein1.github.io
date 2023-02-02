4a193f49aae5532eae26ea088d694b024d0bac1f
Benjamin Klein <Benjaminklein99@gmail.com>
2023-02-02 18:49:22 +0000

    studies

---
+++ Studies/string-manipulation.js
@@ -0,0 +1,19 @@
+/**
+ * String-Manipulation
+ *  0. In Javascript, strings are a datatype that represent words and sentences.  Like most things, they are stored in variables
+ and can be denoted by opening and closing single or double quotes. There are two ways we can change or manipulate strings, methods and operatorss.
+ *  1. Operators in Javascript can somethimes be used for various purposes. It is important to take context into consideration when examining what
+ actions they might perform.
+ *  2. Javascript also has a series of built in methods to manipulate strings. Some of these methods perform the same actions as some operators,
+ and both are equally valid.
+ */
+
+ // 1. Operators //
+ // The syntax for operators is as follows: the name of the string we want to manipulate, the operator, then what we would like to manipulate with.
+ var word1 = 'Hello';
+ var space = ' ';
+ var word2 = 'World';
+ var sentance = word1 + space + word2
+ console.log(sentance) // logs ==> 'Hello World' 
+
+ // 2. Methods //
