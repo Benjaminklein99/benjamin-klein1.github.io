02d3b969b334421f81a1bf53a790e62db9f7c2f7
Benjamin Klein <Benjaminklein99@gmail.com>
2023-02-02 20:30:02 +0000

    done string manip studies

--- Studies/string-manipulation.js
+++ Studies/string-manipulation.js
@@ -5,15 +5,28 @@
  *  1. Operators in Javascript can somethimes be used for various purposes. It is important to take context into consideration when examining what
  actions they might perform.
  *  2. Javascript also has a series of built in methods to manipulate strings. Some of these methods perform the same actions as some operators,
- and both are equally valid.
+ and both are equally valid.  Methods however will sometimes need more information passed to them to get the desired result 
  */
 
  // 1. Operators //
  // The syntax for operators is as follows: the name of the string we want to manipulate, the operator, then what we would like to manipulate with.
+
  var word1 = 'Hello';
  var space = ' ';
  var word2 = 'World';
- var sentance = word1 + space + word2
+ var sentance = word1 + space + word2;
  console.log(sentance) // logs ==> 'Hello World' 
 
+ word1 += space + 'World';
+ console.log(word1); // logs ==> 'Hello World'
+
  // 2. Methods //
+ // The syntax for string methods is the nave of the variable that you would like to manpulate, a dot, the mothod, and parantheses
+ // some methods can be simply invoked without passing anything to their paranthese.
+ var word3 = 'All caps'
+ console.log(word3.toUpperCase()) // logs ==> 'ALL CAPS'
+ 
+
+ // methods such as slice will need further information passed into their parantheses, in this case to denote where to 'cut' our string apart.
+ var word4 = 'slice'
+ console.log(word4.slice(1)) // logs ==> 'lice'
