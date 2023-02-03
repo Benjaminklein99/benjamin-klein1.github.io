43e26f44d6f53858d90466ffbe2c771887c9a093
Benjamin Klein <Benjaminklein99@gmail.com>
2023-02-03 23:04:55 +0000

    done operators fr

--- Studies/operators.js
+++ Studies/operators.js
@@ -30,13 +30,13 @@ console.log(10 / 2); // devition operator: logs ==> 5
 console.log(10 ** 2); // exponential operator: logs ==> 100
 console.log(10 % 3); // remainder operator: logs ==> 1
 console.log(10++); // increment by 1: logs ==> 11
-console.log(10--) // decrement by 1: logs ==> 9
+console.log(10--); // decrement by 1: logs ==> 9
 
 // we can also combine an Aritmetic Operator with an Assignment operator to assign variable to the result of the operation all at once
 var num1 = 10;
 var num2 = 20;
 // instead of this
-num1 = num1 + num2 
+num1 = num1 + num2; 
 console.log(num1); // logs ==> 30
 // we can do this
 num1 += num2;
@@ -52,21 +52,36 @@ console.log( 1 === '1'); // logs ==> false
 console.log( 1 == '1'); // logs ==> true
 
 // we can also compare greater than, less than, greaterthan or equal to, and less than or equal to
-console.log(1 > 2) // logs ==> false
-console.log(1 < 2) // logs ==> true
-console.log(2 >= 1) // logs ==> true
-console.log(2 >= 2) // logs ==> true
+console.log(1 > 2); // logs ==> false
+console.log(1 < 2); // logs ==> true
+console.log(2 >= 1); // logs ==> true
+console.log(2 >= 2); // logs ==> true
 
 
 
-//4. Logical Operators //
+// 4. Logical Operators //
 // There are three types of Logical Operators. They are and (&&), or (||), and not (!). The first, and, is used to compare if both of two statements
 // resolve to true. The or operator is used to evaluate if either of the two statement resolve to true. Finally, the not operator is use to flip the
 // logic of the statement it was used in.  If the statement will resolve to false, it will flip that false into a true and vice versa.
-console.log(1 < 2 && 2 < 3) // logs ==> true
-console.log(1 < 2 && 2 > 3) // logs ==> false
-console.log(1 < 2 || 2 < 3) // logs ==> true
-console.log(1 < 2 || 2 > 3) // logs ==> true
-console.log(1 > 2 || 2 > 3) // logs ==> false
-console.log(1 !== 1) // logs ==> false
-console.log(1 !== 2) // logs ==> true
+console.log(1 < 2 && 2 < 3); // logs ==> true
+console.log(1 < 2 && 2 > 3); // logs ==> false
+console.log(1 < 2 || 2 < 3); // logs ==> true
+console.log(1 < 2 || 2 > 3); // logs ==> true
+console.log(1 > 2 || 2 > 3); // logs ==> false
+console.log(1 !== 1); // logs ==> false
+console.log(1 !== 2); // logs ==> true
+
+// 5. Unary Binary and Ternary Operators//
+// As stated above, operators can be described by the amound of operands they recieve. Operators that take one operand are called Unary, ones 
+// that take two operands are called Binary and ones that take three are Turnary. Here are some example.
+
+// Unary: this example uses the typeof operator wich resolve to the datatype of what follows it
+console.log (typeof 'word') // logs ==> 'string'
+
+// Binary: most of the operators shown above are binary operators
+console.log(1 > 2) // logs ==> false
+
+// Ternary: this example uses the conditional operator (condition ? value1 : value2). If the condition, the first input, is met, the code will
+// resolve to the first value before the colon. Otherwise, it resolves to the second value
+console.log(true ? 1 : 2); // logs ==> 1
+console.log(false ? 1 : 2); // logs ==> 2
